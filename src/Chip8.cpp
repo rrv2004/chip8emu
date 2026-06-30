e1255a545e0a94b4fd8e78ce4c9cf481f4412f17
rrv2004 <rohanaathira22@gmail.com>
2026-06-30 18:54:10 +0530

    defined chip8 class, added template to load ROM

---
+++ src/Chip8.cpp
@@ -0,0 +1,33 @@
+#include "Chip8.h"
+#include <endian.h>
+#include <fstream>
+const unsigned int START_ADDRESS=0x200;
+
+
+//initialize the pc
+Chip8::Chip8(){
+    pc=START_ADDRESS;
+}
+//this loads the contents of the ROM file into the memory
+void Chip8::LoadRom(char const* filename){
+    //open the file as binary streams
+    std::ifstream file(filename, std::ios::binary|std::ios::ate);
+    if(file.is_open()){
+        //get the size of the file
+        std::streampos size=file.tellg();
+        char* buffer=new char[size];
+        //read the file into the buffer
+        file.seekg(0,std::ios::beg);
+        file.read(buffer, size);
+        file.close();
+        //load the buffer into the chip-8 memory
+        // Note: chip 8 starts instructions at 0x200
+        for(long i=0;i<size;i++){
+            memory[START_ADDRESS+i]=buffer[i];
+        }
+        //free the buffer
+        delete [] buffer;
+        
+    }
+    
+}
---
+++ src/Chip8.h
@@ -0,0 +1,21 @@
+//this header defines the chip8 class
+#include <cstdint>
+
+class Chip8{
+private:
+    uint8_t registers[16]{};
+    uint8_t memory[4096]{};
+    uint16_t index_register{};
+    uint16_t pc{};
+    uint16_t stack[16]{};
+    uint8_t sp{};
+    uint8_t delayTimer{};
+    uint8_t soundTimer{};
+    uint8_t keypad[16]{};
+    uint32_t video[64*32]{};
+    uint16_t opcode;
+public:
+    Chip8();
+    void LoadRom(char* filename);
+};
+
